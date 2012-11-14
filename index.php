241b258811aaf3ff7ca056bed94ef1e0bdf5ae89
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-14 12:56:36 +0330

    add footer position to index page

--- index.php
+++ index.php
@@ -9,42 +9,51 @@
 <body>
 	<div class="contain">
 		<div class="container">
-		<header class="header">
-			<div class="log-so">
-				<div class="logo"></div>
-				<div class="social-con">
-					<div class="socialicon"></div>
-					<div class="contact"></div>
+			<header class="header">
+				<div class="log-so">
+					<div class="logo"></div>
+					<div class="social-con">
+						<div class="socialicon"></div>
+						<div class="contact"></div>
+					</div>
+					<div class="badboy"></div>
+					<nav class="menu"></nav>
 				</div>
-				<div class="badboy"></div>
-				<nav class="menu"></nav>
-			</div>
-		</header>
-		<section class="banner">
-			<div class="slideshow"></div>
-			<div class="welcome">
-				<div class="first"></div>
-				<div class="second"></div>
-				<div class="third"></div>
-				<div class="badboy"></div>
-			</div>
-			<div class="recent">
-				<div class="first"></div>
-				<div class="second"></div>
-				<div class="third"></div>
-				<div class="forth"></div>
-				<div class="badboy"></div>
-			</div>
-			<div class="partner">
-				<div class="first"></div>
-				<div class="second"></div>
-				<div class="third"></div>
-				<div class="forth"></div>
-				<div class="fifth"></div>
-				<div class="badboy"></div>
-			</div>
-		</section>
+			</header>
+			<section class="banner">
+				<div class="slideshow"></div>
+				<div class="welcome">
+					<div class="first"></div>
+					<div class="second"></div>
+					<div class="third"></div>
+					<div class="badboy"></div>
+				</div>
+				<div class="recent">
+					<div class="first"></div>
+					<div class="second"></div>
+					<div class="third"></div>
+					<div class="forth"></div>
+					<div class="badboy"></div>
+				</div>
+				<div class="partner">
+					<div class="first"></div>
+					<div class="second"></div>
+					<div class="third"></div>
+					<div class="forth"></div>
+					<div class="fifth"></div>
+					<div class="badboy"></div>
+				</div>
+			</section>
+		</div>
 	</div>
-	</div>
+	<footer class="contain">
+		<div class="container">
+			<div class="first"></div>
+			<div class="second"></div>
+			<div class="third"></div>
+			<div class="forth"></div>
+			<div class="badboy"></div>
+		</div>
+	</footer>
 </body>
 </html>
