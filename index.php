55382c4cfdf746d22598d0770a1643286544a292
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-14 10:31:44 +0330

    add link to css

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="./1styles.css" />
+	<link rel="stylesheet" type="text/css" href="./style.css" />
 </head>
 <body>
-	
+	<div class="container"></div>
 </body>
 </html>
