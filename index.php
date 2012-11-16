77be7e19f108104af9346f086b85aaeb65572fc3
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-16 12:57:15 +0330

    create partner in index page

--- index.php
+++ index.php
@@ -197,11 +197,23 @@
 				</div>
 				<div class="partner">
 					<div class="hline"></div>
-					<div class="first"></div>
-					<div class="second"></div>
-					<div class="third"></div>
-					<div class="forth"></div>
-					<div class="fifth"></div>
+					<ul>
+						<li>
+							<img src="./images/others/logo-05.png" alt="" />
+						</li>
+						<li>
+							<img src="./images/others/logo-04.png" alt="" />
+						</li>
+						<li>
+							<img src="./images/others/logo-03.png" alt="" />
+						</li>
+						<li>
+							<img src="./images/others/logo-02.png" alt="" />
+						</li>
+						<li>
+							<img src="./images/others/logo-01.png" alt="" />
+						</li>
+					</ul>
 					<div class="badboy"></div>
 				</div>
 			</section>
