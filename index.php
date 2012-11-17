7bd7135a5b24de289fe376d2f502f34199b9b337
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-17 12:49:35 +0330

    add script to check corect or incorect text

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="./1styles.css" />
 	<link rel="stylesheet" type="text/css" href="./style.css" />
+	<script type="text/javascript" src="./js/zepto.min.js"></script>
 	<script type="text/javascript" src="./js/jquery.min.js"></script>
 	<script type="text/javascript" src="./js/script.js"></script>
 </head>
