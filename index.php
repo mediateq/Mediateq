881d73d514c77217abcda19f059c9bb2fcac8492
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-17 14:19:11 +0330

    add aboutUs and address to footer

--- index.php
+++ index.php
@@ -228,17 +228,21 @@
 				</div>
 				<div class="contact">
 					<form action="mail.php" id="contact" method="post" target="_blank">
-					<input type="text"  class="text" name="name" id="name" placeholder="نام" />
-					<input type="text" class="text" name="email" id="email" placeholder="ایمیل" />
-					<textarea name="message" id="message" cols="25" rows="5" placeholder="پیام"></textarea>
-					<input type="submit" class="button" id="submit" name="submit" value="ارسال" />
-				</form>
+						<input type="text"  class="text" name="name" id="name" placeholder="نام" />
+						<input type="text" class="text" name="email" id="email" placeholder="ایمیل" />
+						<textarea name="message" id="message" cols="25" rows="5" placeholder="پیام"></textarea>
+						<input type="submit" class="button" id="submit" name="submit" value="ارسال" />
+					</form>
 				<div class="badboy"></div>
 				</div>
 			</div>
 			<div class="second">
 				<div class="hlinefoot">
 					<p>ارتباط با ما</p>
+				</div>
+				<div class="text">
+					<p>آدرس: یلوار شهید مشسنمنمسبن سنمتبنس سینلت نلتمنست ملن ن</p>
+					<p>تلفن: 123456789 </p>
 				</div>
 			</div>
 			<div class="third">
@@ -256,7 +260,9 @@
 				<div class="hlinefoot">
 					<p>درباره ما</p>
 				</div>
-			<div class="badboy"></div>
+				<div class="text">
+					<p>درمنستیلمنتسمتلسمنتلنتسنمنلتمسنتل سشمنیسنمتلسنت سنتشلمنست</p>
+				</div>
 		</div>
 	</footer>
 </body>
