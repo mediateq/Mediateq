013633e3e99012db428dfd21b55e918f1844a17d
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-26 08:50:12 +0330

    add text and boldline

--- contact.php
+++ contact.php
@@ -2,6 +2,15 @@
 	include('./inc/header.php')
 ?>
 
+<div class="content-contact">
+	<div class="text">
+		<p>تماس با ما</p>
+	</div>
+	<div class="hline">
+		<div class="boldline"></div>
+	</div>
+</div>
+
 <?php
 	include('./inc/footer.php')
 ?>
