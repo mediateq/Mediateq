a87e8bc6ce638fcb9a895a7d6a5003df461d8f6a
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-26 19:04:19 +0330

    strat to create aboutUs

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+	include('./inc/header.php')
+?>
+
+<div class="about-content">
+	<div class="tit">
+		<p>در باره ما</p>
+	</div>
+	<div class="hline">
+		<div class="boldline"></div>
+	</div>
+
+
+	
+</div>
+
+<?php
+	include('./inc/footer.php')
+?>
