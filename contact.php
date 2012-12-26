ac0ee8a1e66755850df1a3b6493f31f56b6573dd
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-26 10:36:35 +0330

    create contact form

--- contact.php
+++ contact.php
@@ -16,6 +16,12 @@
 		<div class="hline">
 			<p>فرم تماس</p>
 		</div>
+		<form class="contact-page" id="contact-page" action="mail.php" method="post" target="ifrm">
+			<input type="text"  class="text" name="name" id="name" placeholder="نام" autocomplete="off" />
+			<input type="text" class="text" name="email" id="email" placeholder="ایمیل" autocomplete="off" />
+			<textarea name="message" id="message" cols="25" rows="5" placeholder="پیام"></textarea>
+			<input type="submit" class="button" id="submit" name="submit" value="ارسال" />
+		</form>
 	</div>
 	<div class="left">
 		<div class="hline">
