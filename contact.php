3b4f66c33ce004012ce22e4ec744255d787e7cfa
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-26 10:21:51 +0330

    fix position of contact form and map

--- contact.php
+++ contact.php
@@ -3,12 +3,27 @@
 ?>
 
 <div class="contact-content">
-	<div class="text">
+	<div class="tit">
 		<p>تماس با ما</p>
 	</div>
 	<div class="hline">
 		<div class="boldline"></div>
 	</div>
+	<div class="text">
+		<p>شما می توانید از طریق فرم زیر با ما در تماس باشید. همچنین آدرس و شماره های تماس ما در زیر قید شده است.</p>
+	</div>
+	<div class="right">
+		<div class="hline">
+			<p>فرم تماس</p>
+		</div>
+	</div>
+	<div class="left">
+		<div class="hline">
+			<p>مکان ما</p>
+		</div>
+	</div>
+	<div class="badboy"></div>
+	
 </div>
 
 <?php
