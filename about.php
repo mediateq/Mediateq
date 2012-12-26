d8fceeeb428fed2002b05da87a05bdfd58ee8125
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-27 00:20:36 +0330

    create layout 1 and 2

--- about.php
+++ about.php
@@ -9,9 +9,44 @@
 	<div class="hline">
 		<div class="boldline"></div>
 	</div>
-
-
-	
+	<div class="left">
+		<div class="hline">
+			<p>استخدام</p>
+		</div>
+	</div>	
+	<div class="right">
+		<div class="hline">
+			<p>روش ما</p>
+		</div>
+	</div>
+	<div class="badboy"></div>
+	<div class="meet">
+		<div class="hline">
+			<p>اعضای گروه</p>
+		</div>
+		<div class="first">
+			<img src="./images/about-01.png" alt="">
+			<div class="name"><p>مجتبی امجدی</p></div>
+			<div class="post"><p>طراح سایت</p></div>
+			<div class="hline"></div>
+			<div class="text"><p>شرح واقعه...!</p></div>
+		</div>
+		<div class="second">
+			<img src="./images/about-01.png" alt="">
+			<div class="name"><p>مجتبی امجدی</p></div>
+			<div class="post"><p>طراح سایت</p></div>
+			<div class="hline"></div>
+			<div class="text"><p>شرح واقعه...!</p></div>
+		</div>
+		<div class="third">
+			<img src="./images/about-01.png" alt="">
+			<div class="name"><p>مجتبی امجدی</p></div>
+			<div class="post"><p>طراح سایت</p></div>
+			<div class="hline"></div>
+			<div class="text"><p>شرح واقعه...!</p></div>
+		</div>
+		<div class="badboy"></div>
+	</div>
 </div>
 
 <?php
