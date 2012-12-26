0e09db50044806ebb47d31ae8a036167f868d802
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-26 11:15:10 +0330

    create contact form

--- contact.php
+++ contact.php
@@ -9,19 +9,25 @@
 	<div class="hline">
 		<div class="boldline"></div>
 	</div>
-	<div class="text">
+	<div class="txt">
 		<p>شما می توانید از طریق فرم زیر با ما در تماس باشید. همچنین آدرس و شماره های تماس ما در زیر قید شده است.</p>
 	</div>
 	<div class="right">
 		<div class="hline">
 			<p>فرم تماس</p>
 		</div>
-		<form class="contact-page" id="contact-page" action="mail.php" method="post" target="ifrm">
-			<input type="text"  class="text" name="name" id="name" placeholder="نام" autocomplete="off" />
-			<input type="text" class="text" name="email" id="email" placeholder="ایمیل" autocomplete="off" />
-			<textarea name="message" id="message" cols="25" rows="5" placeholder="پیام"></textarea>
-			<input type="submit" class="button" id="submit" name="submit" value="ارسال" />
-		</form>
+		<div class="contact-page">
+			<form action="mail.php" id="contact" methPwod="post" target="_blank">
+				<p><span>*</span> نام:</p>
+				<input type="text"  class="text" name="name" id="name" autocomplete="off" />
+				<p><span>*</span> ایمیل:</p>
+				<input type="text" class="text" name="email" id="email" autocomplete="off" />
+				<p><span>*</span> پیام:</p>
+				<textarea name="message" id="message" cols="25" rows="5" ></textarea>
+				<input type="submit" class="button" id="submit" name="submit" value="ارسال" />
+			</form>
+			<div class="badboy"></div>
+		</div>
 	</div>
 	<div class="left">
 		<div class="hline">
