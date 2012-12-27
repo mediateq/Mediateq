4d64b7e41514dd0e2e1bf04597c442d891f0e2a6
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-27 12:48:15 +0330

    start to create services page

---
+++ services.php
@@ -0,0 +1,18 @@
+<?php
+	include('./inc/header.php')
+?>
+	<div class="services-content">
+		<div class="tit">
+			<h5>خدمات</h5>
+		</div>
+		<div class="hline">
+			<div class="boldline"></div>
+		</div>
+	</div>
+<?php
+	include('./inc/partner.php')
+?>
+		
+<?php
+	include('./inc/footer.php')
+?>
