85a74d383f77f02d20c62d3b48274f9b320a853e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-30 17:34:29 +0330

    start to create blog page

---
+++ blog.php
@@ -0,0 +1,16 @@
+<?php
+	include('./inc/header.php')
+?>
+
+<div class="blog-content">
+	<div id="tit-otherpage">
+		<h5>وبلاگ</h5>
+	</div>
+	<div class="hline">
+		<div class="boldline"></div>
+	</div>
+</div>
+
+<?php
+	include('./inc/footer.php')
+?>
