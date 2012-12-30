3b7c6b777dd71336df5cc69cd260ae4d22fe8a88
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-30 23:37:04 +0330

    add pageination layout

--- blog.php
+++ blog.php
@@ -38,8 +38,50 @@
 			<p>متن مورد نظر</p>
 		</div>
 		<div class="link">
-			<p><a href="#">ادامه مطلب</a></p>
+			<p><a href="#">ادامه مطلب...</a></p>
 		</div>
+
+		<div class="pic">
+			<img src="./images/blog-img-01.jpg" alt="">
+		</div>
+		<div class="title">
+			<h5><a href="#">سربرگ</a></h5>
+		</div>
+		<div class="detail">
+			<div class="date">
+				<div class="image">
+					<p>تاریخ:</p>
+				</div>
+			</div>
+			<div class="by">
+				<div class="image">
+					<p>توسط:</p>
+				</div>
+			</div>
+			<div class="num-of-com">
+				<div class="image">
+					<p>تعداد:</p>
+				</div>
+			</div>
+			<div class="badboy"></div>
+		</div>
+		<div class="text">
+			<p>متن مورد نظر</p>
+		</div>
+		<div class="link">
+			<p><a href="#">ادامه مطلب...</a></p>
+		</div>
+
+		<div class="hline"></div>
+		<ul class="pages">
+			<li><a href="#"><p>1</p></a></li>
+			<li><a href="#"><p>2</p></a></li>
+			<li><a href="#"><p>3</p></a></li>
+			<li><a href="#"><p>4</p></a></li>
+			<li><a href="#"><p>5</p></a></li>
+		</ul>
+		<div class="badboy"></div>
+
 	</div>
 	<div class="left"></div>
 	<div class="badboy"></div>
