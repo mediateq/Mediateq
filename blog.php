3118e1aa5007888252ae2638bef2ed95d81ec7a4
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-12-30 18:09:42 +0330

    add image to blog

--- blog.php
+++ blog.php
@@ -9,6 +9,16 @@
 	<div class="hline">
 		<div class="boldline"></div>
 	</div>
+	<div class="right">
+		<div class="pic">
+			<img src="./images/blog-img-01.jpg" alt="">
+		</div>
+		<div class="title"></div>
+		<div class="detail"></div>
+		<div class="text"></div>
+		<div class="link"></div>
+	</div>
+	<div class="left"></div>
 </div>
 
 <?php
