5129747992a2c4e3291d9ab14d90a0c83f99d2bd
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-02 11:58:28 +0330

    start to create gallery page

---
+++ gallery.php
@@ -0,0 +1,16 @@
+<?php
+	include('./inc/header.php')
+?>
+
+<div class="gallery-content">
+	<div id="tit-otherpage">
+		<h5>گالری تصاویر</h5>
+	</div>
+	<div class="hline">
+		<div class="boldline"></div>
+	</div>
+</div>
+
+<?php
+	include('./inc/footer.php')
+?>
