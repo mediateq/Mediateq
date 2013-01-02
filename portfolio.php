09d7bad1f78a1ef3c53a3a976b397703fd72ff92
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-02 14:46:08 +0330

    start to create portfolio page

---
+++ portfolio.php
@@ -0,0 +1,17 @@
+<?php
+	include('./inc/header.php')
+?>
+
+<div class="portfolio-content">
+	<div id="tit-otherpage">
+		<h5>نمونه کارها</h5>
+	</div>
+	<div class="hline">
+		<div class="boldline"></div>
+	</div>
+
+</div>
+
+<?php
+	include('./inc/footer.php')
+?>
