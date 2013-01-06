11ba33da621862cb4e7ad13b7ab5325433b06b5e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-06 16:20:47 +0330

    separate content of index

---
+++ inc/index-content.php
@@ -0,0 +1,106 @@
+			<section class="banner">
+				<div class="slideshow">
+					<div id="gallery">
+					    <a class="show">
+						    <img src="images/others/slid1.jpg" alt="Flowing Rock" rel="<h3>سربرگ عکس اول</h3><p>متن عکس اول.</p>"/>
+						</a>  
+					    <a>
+					        <img src="images/others/slid2.jpg" alt="Grass Blades" rel="<h3>سربرگ عکس دوم</h3><p>متن عکس دوم.</p>"/>
+					    </a>
+					    <a>
+					        <img src="images/others/slid3.jpg" alt="Grass Blades" rel="<h3>سربرگ عکس سوم</h3><p>متن عکس سوم.</p>"/>
+					    </a>
+					    <div class="caption">
+					    	<div class="content"></div>
+					    </div>
+					</div>
+					<div class="badboy"></div>
+				</div>
+				<div class="welcome">
+					<div class="first">
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<p>
+							متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+						</p>
+					</div>
+					<div class="second">
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<p>
+							متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+						</p>
+					</div>
+					<div class="third">
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<p>
+							متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+						</p>
+					</div>
+					<div class="badboy"></div>
+				</div>
+				<div class="hline"></div>
+				<div class="recent">
+					<div class="first">
+						<div class="pic">
+							<img src="./images/others/3.jpg" alt="" />
+						</div>
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<div class="hlines"></div>
+						<div class="text">
+							<p>
+								متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+							</p>
+						</div>
+					</div>
+					<div class="second">
+						<div class="pic">
+							<img src="./images/others/4.jpg" alt="" />
+						</div>
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<div class="hlines"></div>
+						<div class="text">
+							<p>
+								متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+							</p>
+						</div>
+					</div>
+					<div class="third">
+						<div class="pic">
+							<img src="./images/others/2.jpg" alt="" />
+						</div>
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<div class="hlines"></div>
+						<div class="text">
+							<p>
+								متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+							</p>
+						</div>
+					</div>
+					<div class="forth">
+						<div class="pic">
+							<img src="./images/others/1.jpg" alt="" />
+						</div>
+						<div class="tit">
+							<p>سربرگ</p>
+						</div>
+						<div class="hlines"></div>
+						<div class="text">
+							<p>
+								متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...متن مورد نظر...
+							</p>
+						</div>
+					</div>
+					<div class="badboy"></div>
+				</div>	
+			</section>
