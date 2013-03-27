5f2a74288427ef02f4d929bb93bb938b01835b2f
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-03-27 12:42:29 +0330

    add partner images

--- inc/partner.php
+++ inc/partner.php
@@ -1,21 +1,21 @@
-		<div class="hline"></div>
-		<div class="partner">
-			<ul>
-				<li>
-					<img src="./images/others/logo-05.png" alt="" />
-				</li>
-				<li>
-					<img src="./images/others/logo-04.png" alt="" />
-				</li>
-				<li>
-					<img src="./images/others/logo-03.png" alt="" />
-				</li>
-				<li>
-					<img src="./images/others/logo-02.png" alt="" />
-				</li>
-				<li>
-					<img src="./images/others/logo-01.png" alt="" />
-				</li>
-			</ul>
-			<div class="badboy"></div>
-		</div>
+<div class="hline"></div>
+<div class="partner">
+	<ul>
+		<li>
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-05.png" alt="" />
+		</li>
+		<li>
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-04.png" alt="" />
+		</li>
+		<li>
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-03.png" alt="" />
+		</li>
+		<li>
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-02.png" alt="" />
+		</li>
+		<li>
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-01.png" alt="" />
+		</li>
+	</ul>
+	<div class="badboy"></div>
+</div>
