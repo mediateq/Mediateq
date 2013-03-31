eec9ae9f304f5893363efc8cdc1a48f9c6765b56
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-03-31 10:17:15 +0330

    fix somth in footer

--- inc/footer.php
+++ inc/footer.php
@@ -40,13 +40,14 @@
 
 							foreach( $recent_posts as $recent ){
 
-								$url     = get_permalink($recent["ID"]);
+								// $url     = get_permalink($recent["ID"]);
+								$url = wp_get_attachment_url( get_post_thumbnail_id($recent["ID"]) );
 								$title   = $recent["post_title"];
 								$content = substr($recent["post_content"], 0, 100);
 								$pict    = get_the_post_thumbnail($recent["ID"], "medium");
 
 								echo "<li>
-										<a href=\"$url\" title=\"$title\"> $pict </a>
+										<a href=\"$url\" title=\"$title\"  rel='prettyphoto[gallery1]'> $pict </a>
 									</li>";
 							}
 						?>
@@ -56,20 +57,21 @@
 				<div class="hlinefoot">
 					<p>کارهای ما</p>
 				</div>
-				<?php $args = array(
-					    'numberposts' => 2,
-					    'offset' => 0,
-					    'category' => 0,
-					    'orderby' => 'post_date',
-					    'order' => 'DESC',
-					    'include' => '',
-					    'exclude' => '',
-					    'meta_key' => '',
-					    'meta_value' =>'',
-					    'post_type' => 'project',
-					    'post_status' => 'draft, publish, future, pending, private',
-					    'suppress_filters' => true 
-					    ); 
+				<ul>
+					<?php $args = array(
+						    'numberposts' => 7,
+						    'offset' => 0,
+						    'category' => 0,
+						    'orderby' => 'post_date',
+						    'order' => 'DESC',
+						    'include' => '',
+						    'exclude' => '',
+						    'meta_key' => '',
+						    'meta_value' =>'',
+						    'post_type' => 'project',
+						    'post_status' => 'draft, publish, future, pending, private',
+						    'suppress_filters' => true 
+						    ); 
 
 						$recent_posts = wp_get_recent_posts( $args );
 
@@ -80,14 +82,12 @@
 							$content = substr($recent["post_content"], 0, 100);
 							$pict    = get_the_post_thumbnail($recent["ID"], "medium");
 
-							echo "<div class='title'>
-	                               	<h5 class='recent-work'><a href=\"$url\"> $title </a></h5>
-								  </div>
-								  <div class='text'>
-									<p class='recent-work'> $content... </p>
-								  </div>";
+							echo "<li>
+									<p class='recent-work'><a class='recent-work' href=\"$url\">$title</a></p>
+								  </li>";
 						}
 					?>
+				</ul>
 			</div>
 			<div class="forth">
 				<div class="hlinefoot">
@@ -100,7 +100,7 @@
 						$link = get_permalink( $page );
 						$content = apply_filters('the_content', $page->post_content);
 						$content = substr($content, 0, 400);
-						echo "<a href=\"$link\">$content...</a>";
+						echo "<a class='footer-about' href=\"$link\">$content...</a>";
 					?>
 				</div>
 			</div>
@@ -114,6 +114,14 @@
 			</div>
 			<div class="badboy"></div>
 		</div>
+		<script type="text/javascript">
+			  $(document).ready(function(){
+			    $("a[rel^='prettyphoto']").prettyPhoto({
+			    	autoplay_slideshow: true,
+			    	show_title: false,
+			    });
+			  });
+		</script>
 	</footer>
 </body>
 </html>
