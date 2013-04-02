e4d59c0069a8821d470cd45e15fcf4d816a1814a
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-04-02 11:45:30 +0330

    check input tag with scripts in weblog

--- js/script.js
+++ js/script.js
@@ -91,7 +91,50 @@ function validateEmail(str){
             return !err;
         });
     });
-})(Zepto);
+
+    $(function(){
+        $('form#commentform').submit(function(){
+            var feild, err=false;
+    
+            target = $('#author');
+    
+            if (validateText( target.val(), 3 )) {
+    
+                target.removeClass('err').addClass('ok');
+    
+            }else{
+    
+                target.removeClass('ok').addClass('err');
+                err= true;
+            }
+    
+            target = $('#email');
+    
+            if (validateEmail(target.val())) { 
+    
+                target.removeClass('err').addClass('ok');
+            }else {
+    
+                target.removeClass('ok').addClass('err');
+                err=true;
+            }
+    
+            target = $('#comment');
+    
+            if (validateText( target.val(), 7 )) {
+    
+                target.removeClass('err').addClass('ok');
+    
+            }else {
+    
+                target.removeClass('ok').addClass("err");
+                err=true;
+            }
+    
+            return !err;
+        });
+    });
+})(jQuery);
 
 
 // Anchor link for scroll
