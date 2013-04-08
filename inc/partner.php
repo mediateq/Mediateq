d306a6aa614eaf62d73957c4598700c4d946838e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-04-08 10:48:48 +0330

    fix alt in images

--- inc/partner.php
+++ inc/partner.php
@@ -2,19 +2,19 @@
 <div class="partner">
 	<ul>
 		<li>
-			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-05.png" alt="" />
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-05.png" alt="mates" />
 		</li>
 		<li>
-			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-04.png" alt="" />
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-04.png" alt="coffee" />
 		</li>
 		<li>
-			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-03.png" alt="" />
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-03.png" alt="hexagon" />
 		</li>
 		<li>
-			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-02.png" alt="" />
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-02.png" alt="king" />
 		</li>
 		<li>
-			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-01.png" alt="" />
+			<img src="<?php bloginfo('template_url') ?>/images/partners/logo-01.png" alt="cubico" />
 		</li>
 	</ul>
 	<div class="badboy"></div>
