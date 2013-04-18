201f347e811ced3f2b7b9c9072f538d6e9fabe37
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-04-18 17:18:49 +0330

    fix search

--- inc/header.php
+++ inc/header.php
@@ -11,16 +11,16 @@
 	<script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/jquery.prettyphoto.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/script.js"></script>
 	<script>
-		  (function() {
-		    var cx = '002646456999169240394:iokkbvidzyg';
-		    var gcse = document.createElement('script');
-		    gcse.type = 'text/javascript';
-		    gcse.async = true;
-		    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-		        '//www.google.com/cse/cse.js?cx=' + cx;
-		    var s = document.getElementsByTagName('script')[0];
-		    s.parentNode.insertBefore(gcse, s);
-		  })();
+	  (function() {
+	    var cx = '002646456999169240394:wwj_bpx_tlk';
+	    var gcse = document.createElement('script');
+	    gcse.type = 'text/javascript';
+	    gcse.async = true;
+	    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+	        '//www.google.com/cse/cse.js?cx=' + cx;
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(gcse, s);
+	  })();
 	</script>
 	<!--[if lt IE 9]>
 		<script src="<?php bloginfo('template_url') ?>/js/html5shiv.js"></script>
